5afaff46371f367d3c10a270437381b60831d306
hi <hi>
2017-07-08 15:13:27 +0800

    Separate Icons and Styles

---
+++ components/IndexComponent/components/RoomsComponent/RoomIcons.js
@@ -0,0 +1,18 @@
+export default {
+    '~default': {
+        'active': require('./images/default-active.png'),
+        'deactive': require('./images/default.png')
+    },
+    'bed': {
+        'active': require('./images/bed-active.png'),
+        'deactive': require('./images/bed.png')
+    },
+    'livingroom': {
+        'active': require('./images/livingroom-active.png'),
+        'deactive': require('./images/livingroom.png')
+    },
+    'kitchen': {
+        'active': require('./images/kitchen-active.png'),
+        'deactive': require('./images/kitchen.png')
+    }
+};
---
+++ components/IndexComponent/components/RoomsComponent/RoomsComponentStyle.js
@@ -0,0 +1,32 @@
+export default {
+    box: {
+        width: 180,
+        backgroundColor: 'white',
+        flex: 1,
+        margin:5,
+        padding:20,
+        borderRadius: 20
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'left'
+    },
+    image: {
+        marginTop: 10
+    },
+    quickInfo: {
+        marginTop: 10,
+        fontWeight: 'bold',
+        color: '#567182'
+    },
+    accessoriesInfo: {
+        color: '#798E9C'
+    }
+};
+
+export const activeStyles = {
+    text: {
+        color: 'rgb(74, 109,249)'
+    }
+}
