13b07ca2f25f373abe9392ffa81b92e9f2671797
hi <hi>
2017-07-08 15:07:25 +0800

    Import styles from stylesheet

--- components/IndexComponent/components/RoomAccessoriesComponent/RoomAccessoriesComponent.js
+++ components/IndexComponent/components/RoomAccessoriesComponent/RoomAccessoriesComponent.js
@@ -3,7 +3,8 @@ import {ScrollView, View, Text} from 'react-native';
 import { AccessoriesComponent } from '../AccessoriesComponent/AccessoriesComponent';
 import { connect } from 'react-redux';
 import { store } from '../../../store';
- 
+import styles from './RoomAccessoriesComponentStyle';
+
 class RoomAccessoriesComponent extends Component {
 
   accessoryChange (accessory) {
@@ -20,44 +21,20 @@ class RoomAccessoriesComponent extends Component {
         onPress={() => this.accessoryChange(x)}
         isactive={x.isactive} />
     );
-
     return accessories
       .filter(x => x.place == currentPlace.key)
       .map(mapCallback);
   }
   render () {
-
-    let styles = {
-      container: {
-        borderRadius:15,
-        backgroundColor: 'white',
-        margin: 5,
-        padding: 15,
-        backgroundColor: 'rgb(39, 85, 249)'        
-      },
-
-      text: {
-        color: 'white',
-        fontSize: 21,
-        fontWeight: 'bold'
-      },
-      accessories: {
-        flexDirection: 'row'
-      }
-    };
-
-
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Room Accessories</Text>
-
         <ScrollView 
           showsHorizontalScrollIndicator={false}
           horizontal={true}
           style={styles.accessories}>
           { this.accessories() }
         </ScrollView>
-
       </View>
     )
   }
