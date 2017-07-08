1451b1a4bf9681f4fc0ec0db899c96c59bcae3e7
Ali Torabi <torabian@users.noreply.github.com>
2017-07-09 02:07:11 +0800

    Add react native slider (#11)

--- components/IndexComponent/components/TemperatureComponent/TemperatureComponent.js
+++ components/IndexComponent/components/TemperatureComponent/TemperatureComponent.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
-import {ScrollView, View, Text, Slider} from 'react-native';
+import {ScrollView, View, Text} from 'react-native';
 import { AccessoriesComponent } from '../AccessoriesComponent/AccessoriesComponent';
 import style from './TemperatureComponentStyle';
+import Slider from 'react-native-slider';
+
+var customStyles2 = {
+  track: {
+    height: 4,
+    borderRadius: 2,
+  },
+  thumb: {
+    width: 30,
+    height: 30,
+    borderRadius: 30 / 2,
+    backgroundColor: 'white',
+    borderColor: '#30a935',
+    borderWidth: 2,
+  }
+} ;
+
 
 export default function TemperatureAdjustComponent ({accessory, updateValue}) {
     return (
@@ -15,6 +32,8 @@ export default function TemperatureAdjustComponent ({accessory, updateValue}) {
           step={0.5}
           value={accessory.value}
           maximumValue={60}
+          trackStyle={customStyles2.track}
+          thumbStyle={customStyles2.thumb}
           onValueChange={(value) => {
             updateValue(accessory, value)
           }} />
